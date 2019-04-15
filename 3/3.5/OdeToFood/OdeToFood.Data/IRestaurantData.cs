124dc719a261ed029eeec6f75ad121ce73a792de
DeanWebbDeveloper <dean@deanwebbdeveloper.com>
2019-04-15 15:34:23 +0100

    2.6 - 3.8
    Chapter 2 completed,
    Up to 3.8 completed

---
+++ 3/3.5/OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,34 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Zizzi's", Location = "Nottingham", Cuisine = CuisineType.Italian },
+                new Restaurant { Id = 2, Name = "Tamatanga", Location = "London", Cuisine = CuisineType.Indian },
+                new Restaurant { Id = 3, Name = "Gourmet Burger Kitchen", Location = "Las Iguanas", Cuisine = CuisineType.Mexican }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
